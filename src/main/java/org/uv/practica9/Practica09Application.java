29437eefda6c5f7bbeb1df3e1f0d6debb790e80e
IvanAvila1224 <magista1224@gmail.com>
2023-05-06 15:59:28 -0500

    CRUD

--- src/main/java/org/uv/practica9/Practica09Application.java
+++ src/main/java/org/uv/practica9/Practica09Application.java
@@ -1,5 +1,9 @@
 package org.uv.practica9;
 
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
---
+++ src/main/java/org/uv/practica9/controller/RepositoryEmpleado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.uv.practica9.controller;
+
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ *
+ * @author btoarriola
+ */
+public interface RepositoryEmpleado extends CrudRepository<Empleado, Long>{
+    
+}
